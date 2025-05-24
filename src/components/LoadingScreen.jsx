6f02863f91e26d72a6f025ad69e91d67e491038f
Erik <erik.sandqvist@keylinx.se>
2025-05-24 17:56:10 +0200

    Init

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+export const LoadingScreen = () => {
+  return (
+    <div className="fixed inset-0 z-50 bg-black text-yellow-100 flex flex-col items-center justify-center">
+      <div className="mb-4 text-4xl font-mono font-bold"></div>
+      Tja<span className="animate-blink ml-1"> | </span>
+
+      <div className="w-[200px h-[2px bg-gray-800 rounded relative overflow-hidden">
+<div> className</div>
+      </div>
+    </div>
+  );
+}
