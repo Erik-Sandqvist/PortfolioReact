a6861a0dd47f87e64ac86f2a445b9cd0f7cef8cf
Erik <104572224+Erik-Sandqvist@users.noreply.github.com>
2025-05-24 21:55:26 +0200

    txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
   return (
-  <>
-  <LoadingScreen /> 
-  </>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? "opacity-100" : "opacity-0"
+        } bg- #081A3A text-#D3B60A-100`}>
+        
+      </div>
+    </>
   );
 }
 
