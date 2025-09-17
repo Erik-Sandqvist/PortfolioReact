41404fecb31fa3451d48148fbbd183b193778943
Erik <104572224+Erik-Sandqvist@users.noreply.github.com>
2025-09-17 19:49:18 +0200

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import ghPages404 from './vite-plugin-ghpages-404.js'
 
 export default defineConfig({
-  base: '/PortfolioReact/',
+  base: '/',
   plugins: [react(), ghPages404()],
 })
