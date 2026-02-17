b63f0c0b44ba0976338260dbb89fbfc86865e702
Erik <esandqvist04@gmail.com>
2026-02-17 19:42:36 +0100

    small update

--- src/components/projects/nocco.jsx
+++ src/components/projects/nocco.jsx
@@ -11,6 +11,7 @@ const Covers = () => {
 
     return (
         <>
+        <div className="h-full w-full bg-gradient-to-b bg-gradient-to-br from-base via-red-900 to-black flex flex-col items-center justify-center">
         <div className="max-w-7xl mx-auto mt-24 mb-20 px-6">
             <h2 className="text-3xl font-bold text-primary mb-2">Nocco Posters</h2>
             <p className="text-text-primary mb-8">More Comming...</p>
@@ -53,6 +54,7 @@ const Covers = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
                 </Link>
             </div>
+             </div>
         </>
     );
 };
